21d3b9b6465bbac43908bdc9955c5831083ad90c
SASI2492 <48341414+SASI2492@users.noreply.github.com>
2019-03-08 08:55:30 +0530

    Add files via upload

---
+++ resumeprg.py
@@ -0,0 +1,13 @@
+a="Resume program using python"
+new_string= a.center(40,"-")
+print(new_string)
+fn=input("enter your first name: ")
+ln=input("enter your last name : ")
+mn=input("enter your middle name: ")
+x=input("enter your email id: ")
+y=input("enter your address: ")
+print(fn)
+print(ln)
+print(mn)
+print(x)
+print(y)
